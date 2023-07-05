4ea2972345b50fe7e730b88b7875c2bf3fb4e0bb
Nguyentuonghien <hientuong10101997@gmail.com>
2023-07-05 11:40:16 +0700

    fix error message with axios interceptor

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,10 +1,17 @@
+import { ToastContainer } from 'react-toastify'
 import './App.css'
 import useRoutElements from './useRoutElements'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElemets = useRoutElements() // custom Hook
 
-  return <div>{routeElemets}</div>
+  return (
+    <div>
+      {routeElemets}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
--- ShopeeClone/src/utils/http.ts
+++ ShopeeClone/src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from 'src/constants/HttpStatusCode.enum'
+import { toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
@@ -11,6 +13,24 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    // add 1 response interceptor:
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        // console.log('error: ', error)
+        // xử lý các lỗi khác lỗi 422
+        if (error.response?.status != HttpStatusCode.UnprocessableEntity) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data // trick
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
